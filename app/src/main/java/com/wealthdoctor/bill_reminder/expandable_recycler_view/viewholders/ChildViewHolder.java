57bf01b7ec0ead5a7292f5303ca6b41c8d4916ee
Chandan <chandan115325@gmail.com>
2018-01-24 18:28:40 +0530

    24th Jan evening

--- app/src/main/java/com/wealthdoctor/bill_reminder/expandable_recycler_view/viewholders/ChildViewHolder.java
+++ app/src/main/java/com/wealthdoctor/bill_reminder/expandable_recycler_view/viewholders/ChildViewHolder.java
@@ -1,5 +1,7 @@
 package com.wealthdoctor.bill_reminder.expandable_recycler_view.viewholders;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
@@ -10,50 +12,57 @@ import android.widget.Toast;
 
 import com.wealthdoctor.R;
 import com.wealthdoctor.bill_reminder.activity.BillReminderActivity;
+import com.wealthdoctor.bill_reminder.activity.BillReminderDetailEditActivity;
 
 
-public class ChildViewHolder extends RecyclerView.ViewHolder  {
+public class ChildViewHolder extends RecyclerView.ViewHolder {
 
-  TextView edit;
-  ImageView editIcon;
-  ImageView deleteIcon;
-  CheckBox alreadyPaid;
+    TextView edit;
+    ImageView editIcon;
+    ImageView deleteIcon;
+    CheckBox alreadyPaid;
+    private Context mContext;
 
-  public ChildViewHolder(View itemView) {
-    super(itemView);
+    public ChildViewHolder(View itemView) {
+        super(itemView);
 
-    edit = (TextView)itemView.findViewById(R.id.list_item_child);
-    editIcon = (ImageView)itemView.findViewById(R.id.child_edit);
-    deleteIcon = (ImageView)itemView.findViewById(R.id.child_delete);
-    alreadyPaid = (CheckBox) itemView.findViewById(R.id.child_already_paid);
+        edit = (TextView) itemView.findViewById(R.id.list_item_child);
+        editIcon = (ImageView) itemView.findViewById(R.id.child_edit);
+        deleteIcon = (ImageView) itemView.findViewById(R.id.child_delete);
+        alreadyPaid = (CheckBox) itemView.findViewById(R.id.child_already_paid);
 
 
-    edit.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-           // Log.d("BillReminder", "edit is working");
-        }
-    });
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, BillReminderDetailEditActivity.class);
+                mContext.startActivity(intent);
+                Log.d("BillReminder", "edit is working");
 
-    editIcon.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Log.d("BillReminder", "Edit Icon working");
-        }
-    });
+            }
+        });
 
-    deleteIcon.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Log.d("BillReminder", "Delete Icon working");
-        }
-    });
+        editIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("BillReminder", "Edit Icon working");
+            }
+        });
 
-    alreadyPaid.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Log.d("BillReminder", "Checkbox is working");
-        }
-    });
-  }
+        deleteIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("BillReminder", "Delete Icon working");
+
+            }
+        });
+
+        alreadyPaid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("BillReminder", "Checkbox is working");
+            }
+        });
+    }
 }
